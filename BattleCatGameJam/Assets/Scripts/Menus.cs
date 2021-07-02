0a85d8a52d963a5a8fd84b402501d8b7453e5a97
fuwwyUwU <vsjoqvist@gmail.com>
2021-07-03 00:17:15 +0200

    pause menu pauses game

--- BattleCatGameJam/Assets/Scripts/Menus.cs
+++ BattleCatGameJam/Assets/Scripts/Menus.cs
@@ -10,11 +10,14 @@ namespace BattleCat
         public bool playing = true;
         [SerializeField] GameObject pauseMenu;
         Rigidbody2D playerRb;
+        GameObject player;
+        Vector2 pausePlace;
 
         // Start is called before the first frame update
         void Start()
         {
-        
+            playerRb = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+            player = GameObject.Find("Player");
         }
 
         // Update is called once per frame
@@ -26,16 +29,27 @@ namespace BattleCat
                 {
                     pauseMenu.SetActive(false);
                     playing = true;
+                    playerRb.isKinematic = false;
 
                 }
                 else if (pauseMenu.activeInHierarchy == false)
                 {
                     pauseMenu.SetActive(true); 
                     playing = false;
+                    playerRb.isKinematic = true;
+                    pausePlace = player.transform.position;
                 }
             }
         }
-    }
 
-    
+        private void FixedUpdate()
+        {
+            if (!playing)
+            {
+                player.transform.position = pausePlace;
+            }
+        }
+
+
+    }
 }
