ff7039d15a3cbe3d6457966b54ab10d89746e202
fuwwyUwU <vsjoqvist@gmail.com>
2021-07-02 19:05:52 +0200

    qol for jumping

--- BattleCatGameJam/Assets/Scripts/PlayerScript.cs
+++ BattleCatGameJam/Assets/Scripts/PlayerScript.cs
@@ -7,18 +7,20 @@ public class PlayerScript : MonoBehaviour
 
     [SerializeField] float speed = 1;
     [SerializeField] float jump = 5;
-    bool allowJump;
+    [SerializeField] bool allowJump;
     Rigidbody2D playerRb;
+    bool isOnGround;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        allowJump = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        allowJump = true;
+
     }
 
     private void FixedUpdate()
@@ -26,7 +28,17 @@ public class PlayerScript : MonoBehaviour
         //jump
         if (allowJump && Input.GetKey(KeyCode.Space)){
 
-            playerRb.velocity = Vector2.up * jump;
+            if (isOnGround)
+            {
+                playerRb.velocity = Vector2.up * jump;
+                allowJump = false;
+            }
+            else
+            {
+                StartCoroutine(CheckForJump());
+            } 
+
+
         }
         //walk right
         if (Input.GetKey(KeyCode.D))
@@ -41,14 +53,31 @@ public class PlayerScript : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision)
     {
+        //on collions stay
         allowJump = true;
+        isOnGround = true;
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
+        //when you exit the collion
         StartCoroutine(CoyoteTime());
+        isOnGround = false;
     }
 
+    IEnumerator CheckForJump()
+    {
+        //remeber jumps if they are 1 frame befor you can jump
+        
+        yield return new WaitForSeconds(0.1666f);
+        if (allowJump)
+        {
+            playerRb.velocity = Vector2.up * jump;
+            allowJump = false;
+        }
+    }
+
+    //allows jumps for 3 more frames after they jumped
     IEnumerator CoyoteTime()
     {
         yield return new WaitForSeconds(0.05f);
