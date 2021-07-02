090b5105aed973b2f9ec1c034ca86eaaad717794
fuwwyUwU <vsjoqvist@gmail.com>
2021-07-02 18:17:49 +0200

    movement
    added movement

--- BattleCatGameJam/Assets/Scripts/PlayerScript.cs
+++ BattleCatGameJam/Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+
+    float speed = 1;
+    float jump = 5;
+    bool allowJump;
+    Rigidbody2D playerRb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    private void FixedUpdate()
+    {
+        if (allowJump && Input.GetKeyDown(KeyCode.Space)){
+
+            playerRb.velocity = Vector2.up * jump;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
     }
 }
