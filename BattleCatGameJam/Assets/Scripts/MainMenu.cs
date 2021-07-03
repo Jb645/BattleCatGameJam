1b7183cf4080c9cd29a204d142a7d1e3c4b77b5b
fuwwyUwU <vsjoqvist@gmail.com>
2021-07-03 15:25:51 +0200

    fixed mainmenu

--- BattleCatGameJam/Assets/Scripts/MainMenu.cs
+++ BattleCatGameJam/Assets/Scripts/MainMenu.cs
@@ -15,7 +15,7 @@ namespace BattleCat
         [SerializeField] GameObject credits;
 
         // Start is called before the first frame update
-        void Start()
+        void Start()  
         {
         
         }
